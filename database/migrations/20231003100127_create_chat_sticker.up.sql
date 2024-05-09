4a1fe0ef552c2406926eef4a3901d9790e485576
robert <juroberttyb@gmail.com>
2024-05-09 10:58:13 +0800

    update: related fields to column name change

--- database/migrations/20231003100127_create_chat_sticker.up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS public.chat_sticker
-(
-    id character varying(20) COLLATE pg_catalog."default" NOT NULL,
-    name character varying(50) COLLATE pg_catalog."default" NOT NULL,
-    folder character varying(20) COLLATE pg_catalog."default" NOT NULL,
-    line_url character varying(320) COLLATE pg_catalog."default" NOT NULL,
-    created_at timestamp without time zone NOT NULL,
-    CONSTRAINT chat_sticker_pkey PRIMARY KEY (id)
-);
