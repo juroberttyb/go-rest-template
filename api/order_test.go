15159f97f09e8ee6b1577eb91cdd64bf088de555
robert <juroberttyb@gmail.com>
2024-05-13 23:23:12 +0800

    add: more logging in api test case

--- api/order_test.go
+++ api/order_test.go
@@ -1,22 +1,14 @@
 package api
 
 import (
-	"context"
 	"encoding/json"
 	"io"
 	"log"
 	"net/http"
-	"os"
 	"testing"
 
-	"github.com/A-pen-app/cache"
-	"github.com/A-pen-app/kickstart/config"
-	"github.com/A-pen-app/kickstart/database"
-	"github.com/A-pen-app/kickstart/global"
 	"github.com/A-pen-app/kickstart/models"
 	"github.com/A-pen-app/kickstart/tests"
-	"github.com/A-pen-app/logging"
-	"github.com/A-pen-app/tracing"
 	"github.com/stretchr/testify/require"
 )
 
@@ -25,74 +17,9 @@ func TestGetBoardAPIIntegration(t *testing.T) {
 		t.Skip("skipping system integration test")
 	}
 
-	os.Setenv("TESTING", "true") // to inform different parts of the application that we are testing and perform accordingly
-
-	log.Println("Initializing resource for testing...")
-	// We're running, turn on the liveness indication flag.
-	global.Alive = true
-
-	// Create root context.
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	var projectID string = config.GetString("PROJECT_ID")
-	if !config.GetBool("PRODUCTION_ENVIRONMENT") {
-		projectID = ""
+	if tests.BaseURL == tests.DEFAULT_DEV_DEPLOY_URL {
+		t.Skip("skipping api integration test; base URL not set")
 	}
-
-	// Setup logging module.
-	// NOTE: This should always be first.
-	if err := logging.Initialize(&logging.Config{
-		ProjectID:    projectID,
-		Level:        logging.Level(config.GetUint("LOG_LEVEL")),
-		Development:  !config.GetBool("PRODUCTION_ENVIRONMENT"),
-		KeyRequestID: "request_id",
-		KeyUserID:    "user_id",
-		KeyError:     "err",
-		KeyScope:     "scope",
-	}); err != nil {
-		panic(err)
-	}
-	defer logging.Finalize()
-
-	// Setup tracing module
-	env := "development"
-	if config.GetBool("PRODUCTION_ENVIRONMENT") {
-		env = "production"
-	}
-	tracing.Initialize(ctx, &tracing.Config{
-		ProjectID:             config.GetString("PROJECT_ID"),
-		TracerName:            "kickstart",
-		ServiceName:           global.ServiceName,
-		DeploymentEnvironment: env,
-	})
-	defer tracing.Finalize(ctx)
-
-	// Setup cache module
-	//FIXME set it to production for testing purpose
-	cacheType := cache.TypeLocal
-	prefix := "local-dev"
-	redisURL := "localhost:6379"
-
-	if config.GetBool("PRODUCTION_ENVIRONMENT") {
-		cacheType = cache.TypeRedis
-		redisURL = "10.49.162.163:6379"
-		prefix = config.GetString("SERVICE_NAME")
-	}
-	cache.Initialize(&cache.Config{
-		Type:     cacheType,
-		RedisURL: redisURL,
-		Prefix:   prefix,
-	})
-	defer cache.Finalize()
-
-	// Setup database module.
-	database.Initialize(ctx)
-	defer database.Finalize()
-
-	// Now that we finished initializing all necessary modules,
-	// let's turn on the readiness indication flag.
-	global.Ready = true
 
 	log.Println("Resource for testing initialized...")
 	url := tests.BaseURL + "/board?board_type=live"
